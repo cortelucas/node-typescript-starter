6b87b3d4c315bbe5f0709728163c3d93e2c83916
Lucas Corte <corte1994.lc@gmail.com>
2021-08-04 08:53:03 -0300

    MOdel do user

---
+++ src/models/User.Model.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+  }
+});
+
+export default model('Usuário', userSchema);
